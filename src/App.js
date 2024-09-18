25dc4ec0887c9a2df5d93b4d0d79bb663bf16448
zinzindev <zinzin30@windowslive.com>
2024-09-18 12:21:57 +0700

    finish sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { FaHome } from 'react-icons/fa';
+import { FaHome, FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
 
 function App() {
 	return (
@@ -13,7 +13,52 @@ function App() {
 			</header>
 
 			{/* Sidebar */}
-			<aside className='sidebar'>Sidebar</aside>
+			<aside className='sidebar'>
+				<section className='sidebar__generic'>
+					<ul className='generic__lists'>
+						<li className='active'>
+							<span>
+								<FaInbox />
+							</span>
+							<h6>Inbox</h6>
+						</li>
+						<li>
+							<span>
+								<FaCalendar />
+							</span>
+							<h6>Today</h6>
+						</li>
+						<li>
+							<span>
+								<FaCalendarAlt />
+							</span>
+							<h6>Next 7 Days</h6>
+						</li>
+					</ul>
+				</section>
+				<section className='sidebar__middle'>
+					<span>
+						<FaChevronDown />
+					</span>
+					<h6>Projects</h6>
+				</section>
+				<section className='sidebar__projects'>
+					<ul className='project__lists'>
+						<li>
+							<span>
+								<FaInbox />
+							</span>
+							<h6>Project-A</h6>
+						</li>
+						<li>
+							<span>
+								<FaCalendar />
+							</span>
+							<h6>Project-B</h6>
+						</li>
+					</ul>
+				</section>
+			</aside>
 
 			{/* Content */}
 			<main className='content'>Content</main>
