259013e53ecb94aa4b53745f58c629b57e37dbb9
zinzindev <zinzin30@windowslive.com>
2024-09-18 14:03:01 +0700

    finish component Headers&Sidebar

--- src/App.js
+++ src/App.js
@@ -1,65 +1,12 @@
 import './App.scss';
-import { FaHome, FaInbox, FaCalendar, FaCalendarAlt, FaChevronDown } from 'react-icons/fa';
+import { Header } from './components/Headers.js';
+import { Sidebar } from './components/Sidebar.js';
 
 function App() {
 	return (
 		<div className='container'>
-			{/* Header */}
-			<header className='header'>
-				<span>
-					<FaHome />
-				</span>
-				<h3>CC14-Todoist</h3>
-			</header>
-
-			{/* Sidebar */}
-			<aside className='sidebar'>
-				<section className='sidebar__generic'>
-					<ul className='generic__lists'>
-						<li className='active'>
-							<span>
-								<FaInbox />
-							</span>
-							<h6>Inbox</h6>
-						</li>
-						<li>
-							<span>
-								<FaCalendar />
-							</span>
-							<h6>Today</h6>
-						</li>
-						<li>
-							<span>
-								<FaCalendarAlt />
-							</span>
-							<h6>Next 7 Days</h6>
-						</li>
-					</ul>
-				</section>
-				<section className='sidebar__middle'>
-					<span>
-						<FaChevronDown />
-					</span>
-					<h6>Projects</h6>
-				</section>
-				<section className='sidebar__projects'>
-					<ul className='project__lists'>
-						<li>
-							<span>
-								<FaInbox />
-							</span>
-							<h6>Project-A</h6>
-						</li>
-						<li>
-							<span>
-								<FaCalendar />
-							</span>
-							<h6>Project-B</h6>
-						</li>
-					</ul>
-				</section>
-			</aside>
-
+			<Header />
+			<Sidebar />
 			{/* Content */}
 			<main className='content'>Content</main>
 		</div>
