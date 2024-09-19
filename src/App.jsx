7e99a444f556e86b06e8f3978d16ca7027ef5bbb
zinzindev <zinzin30@windowslive.com>
2024-09-19 15:29:39 +0700

    refrefactor TodoContent

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Header } from './components/Header.jsx';
+import { Sidebar } from './components/Sidebar.jsx';
+import { TodoContent } from './components/Todo/TodoContent.jsx';
+
+function App() {
+	return (
+		<div className='container'>
+			<Header />
+			<Sidebar />
+			<TodoContent />
+		</div>
+	);
+}
+
+export default App;
