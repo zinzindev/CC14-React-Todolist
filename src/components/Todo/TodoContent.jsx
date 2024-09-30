a7464000fc4a54378052e328011bd89ffedcd016
zinzindev <zinzin30@windowslive.com>
2024-09-30 22:23:04 +0700

    finish: add-todo function

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { AddTodo } from './AddTodo';
 import { TodoHeader } from './TodoHeader';
@@ -9,13 +10,29 @@ import mockData from '../../data/todos.json';
 export function TodoContent() {
 	// # Logic
 	const [todos, setTodos] = useState(mockData);
-	// console.log(todos);
+	// console.log(uuidv4());
+	// console.log(uuidv4());
+	// console.log(uuidv4());
+
+	const handleAddTodo = () => {
+		// มี new todos
+		let newTodoObj = { id: uuidv4(), task: 'Do Hw', status: false, due_date: '' };
+
+		// สร้าง state ใหม่
+		// update state โดย new state
+		// const newTodos = [newTodoObj, ...todos];
+		// setTodos(newTodos);
+
+		// update state โดย callback
+		setTodos((currentState) => [newTodoObj, ...currentState]);
+	};
 
 	// # UI
 	return (
 		<main className='content'>
 			<TodoHeader />
 			<AddTodo />
+			<button onClick={handleAddTodo}>Test Add Todo</button>
 			<TodoList todos={todos} />
 		</main>
 	);
